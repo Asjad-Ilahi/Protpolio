a0264a296abfaaf5b3b2b723a64f0c2255392c19
Asjad Ilahi <160521387+Asjad-Ilahi@users.noreply.github.com>
2025-01-24 00:57:36 +0500

    now show time

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,16 @@ import 'package:game_protpolio/protpolio.dart';
 
 void main(){
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(GameWidget(game: Portfolio()));
+  runApp(MaterialApp(home: GameScreen(),));
+}
+
+class GameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GameWidget(
+        game: Portfolio(), // Pass context here
+      ),
+    );
+  }
 }
--- lib/protpolio.dart
+++ lib/protpolio.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 
 import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
@@ -10,49 +11,54 @@ import 'package:flutter/services.dart';
 import 'package:game_protpolio/character/main_character.dart';
 import 'package:game_protpolio/ground.dart';
 
-class Portfolio extends FlameGame with HasCollisionDetection,KeyboardEvents{
+class Portfolio extends FlameGame with HasCollisionDetection, KeyboardEvents {
   late MainCharacter man;
   late Rectangle levelBounds;
-  bool movement = false;
   late TiledComponent background1;
   late final CameraComponent cameraComponent;
   late final World world;
   List<Ground> collisionBlocks = [];
 
-
   @override
   Future<void> onLoad() async {
     super.onLoad();
 
+    // Load the Tiled map
     background1 = await TiledComponent.load('tiled.tmx', Vector2.all(32));
     add(background1);
 
-    levelBounds =  Rectangle.fromPoints(
-      Vector2(
-        0,
-        0,
-      ),
+    // Define the level boundaries based on the Tiled map dimensions
+    levelBounds = Rectangle.fromPoints(
+      Vector2(0, 0),
       Vector2(
         background1.tileMap.map.width.toDouble(),
         background1.tileMap.map.height.toDouble(),
-      ) *
-          32,
+      ) * 32,
     );
-    man = MainCharacter(levelBounds: levelBounds);
-    man
-      ..size = Vector2(50,52)..position = Vector2(300, 544);
 
-    double width = (32.0 * background1.tileMap.map.width);
-    world = World(children: [background1,man]);
+    // Initialize the main character
+    man = MainCharacter(levelBounds: levelBounds)
+      ..size = Vector2(64, 64)
+      ..position = Vector2(300, 544);
+
+    // Create the game world and add components
+    world = World(children: [background1, man]);
     _addCollisions();
-    cameraComponent = CameraComponent(
+
+    // Set up the camera with a fixed width and bottom alignment
+    double maxSide = min(size.x, size.y);
+    cameraComponent = CameraComponent.withFixedResolution(
       world: world,
-    )..viewport.size = Vector2(450, 50)
-      ..viewfinder.position = man.position
-    ..viewfinder.anchor = Anchor.topLeft
-    ..viewport.position = Vector2(300, 0);
+      width: maxSide,
+      height: maxSide, // Dynamically calculate height
+    );
+    cameraComponent.viewfinder.anchor = const Anchor(0.5, 1) ;
+    size.x > 700 ? cameraComponent.viewfinder.zoom = 1.4 : cameraComponent.viewfinder.zoom = 1;
+
     cameraComponent.follow(man);
-    cameraComponent.setBounds(Rectangle.fromPoints(levelBounds.topRight, levelBounds.topLeft));
+    cameraComponent.setBounds(Rectangle.fromPoints(levelBounds.bottomLeft,levelBounds.bottomRight));
+
+    // Add the world and camera to the game
     await addAll([world, cameraComponent]);
   }
 
@@ -62,45 +68,45 @@ class Portfolio extends FlameGame with HasCollisionDetection,KeyboardEvents{
     man.updatePlayerState();
     man.updatePlayerMovement(dt);
     man.positionUpdate(dt);
-
-
     man.checkHorizontalCollisions();
     man.applyGravity(dt);
     man.checkVerticalCollisions();
   }
 
-
   void _addCollisions() {
     final obstacleGroup = background1.tileMap.getLayer<ObjectGroup>('ground');
 
-    for(final obj in obstacleGroup!.objects){
-      final block = Ground(position: Vector2(obj.x,obj.y), size: Vector2(obj.width,obj.height),isPlatform: true);
+    for (final obj in obstacleGroup!.objects) {
+      final block = Ground(
+        position: Vector2(obj.x, obj.y),
+        size: Vector2(obj.width, obj.height),
+        isPlatform: true,
+      );
       collisionBlocks.add(block);
       world.add(block);
     }
     man.ground = collisionBlocks;
   }
+
   @override
   KeyEventResult onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
-      man.horizontalMovement=0;
-      final keyLeft = keysPressed.contains(LogicalKeyboardKey.arrowLeft);
-      final keyRight = keysPressed.contains(LogicalKeyboardKey.arrowRight);
-      final keyUp = keysPressed.contains(LogicalKeyboardKey.arrowUp);
-      if (keyLeft) {
-        man.horizontalMovement = -1;
-      }
-      if (keyRight) {
-        man.horizontalMovement = 1;
-      }
+    man.horizontalMovement = 0;
+    final keyLeft = keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final keyRight = keysPressed.contains(LogicalKeyboardKey.arrowRight);
+    final keyUp = keysPressed.contains(LogicalKeyboardKey.arrowUp);
 
-      if(keyUp){
-        man.hasJumped =true;
-        man.playerJump(0);
-      }
+    if (keyLeft) {
+      man.horizontalMovement = -1;
+    }
+    if (keyRight) {
+      man.horizontalMovement = 1;
+    }
+    if (keyUp) {
+      man.hasJumped = true;
+      man.playerJump(0);
+    }
 
     super.onKeyEvent(event, keysPressed);
-
     return KeyEventResult.handled;
   }
-
 }
